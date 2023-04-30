05131843ff2c112f0810903448856a6535c2662b
hysteria222 <hgunter22206@gmail.com>
2023-04-29 21:55:58 -0700

    Pushing challenge.

--- index.js
+++ index.js
@@ -30,9 +30,9 @@ console.log('example task:', processFirstItem(['foo','bar'],function(str){return
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
-  
+    one has a closure and a nest function and one doesn't
   2. Which of the two uses a closure? How can you tell?
-  
+  counter one, because it has a nested function and pulls from the outside function to find variables 
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
 */
@@ -64,10 +64,14 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(){
+  return Math.floor(Math.random()*3) 
+   
 }
 
+
+
+console.log(inning())
 
 /* ⚾️⚾️⚾️ Task 3: finalScore() ⚾️⚾️⚾️
 Use the finalScore function below to do the following:
@@ -83,10 +87,19 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*Code Here*/){
-  /*Code Here*/
+function finalScore(inning, numberOf){
+let  Home  = 0
+let Away  = 0
+//  console.log("homeScore:", homeScore)
+//  console.log("awayScore:", awayScore)
+ for (let i = 0; i < numberOf; i++){
+  Home = inning() + Home
+  Away  = inning() + Away
+ }
+ return {Home, Away}
 }
 
+console.log(finalScore(inning, 2))
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
@@ -101,11 +114,16 @@ For example: invoking getInningScore(inning) might return this object:
   */
 
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(inning) {
+let Home = 0
+let Away = 0 
+Home = inning() + Home
+Away = inning() + Away
+return {Home, Away}
 
 }
 
+console.log(getInningScore(inning))
 
 /* STRETCH: ⚾️⚾️⚾️ Task 5: scoreboard() ⚾️⚾️⚾️
 Use the scoreboard function below to do the following:
